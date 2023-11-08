d374df5a6ca68c364ae1aeefb6b35209ec77cf79
lfersoy <lfersoy.work@gmail.com>
2023-11-08 11:22:22 -0500

    feat / challenge-01

---
+++ 2023/challenge01/challenge01.md
@@ -0,0 +1,46 @@
+# Challengue #01
+
+** The Challenge **
+A spy is sending encrypted messages.
+
+Your mission is to create a program that decodes the messages.
+
+The messages are words separated by spaces like this:
+cat dog dog car Cat doG sun
+
+We need the program to return the number of times each word appears in the message, regardless of whether it is in uppercase or lowercase.
+
+The result will be a text string with the word and the number of times it appears in the message, in this format:
+```cat2dog3car1sun1```
+
+The words are sorted by their first appearance in the message!
+
+** More Examples: **
+```
+keys house HOUSE house keys -> keys2house3
+cup te a cup -> cup2te1a1
+houses house housess -> houses1house1housess1
+```
+
+## Solución
+
+```php
+function countWords($message) {
+  $info = explode(" ", $message);
+  $aux = [];
+  return array_reduce($info, function ($carry, $word) use (&$aux) {
+    $word = strtolower($word);
+
+    if (isset($aux[$word])) {
+      $carry = str_replace("{$word}{$aux[$word]}", $word . $aux[$word] + 1, $carry);
+      $aux[$word] += 1;
+    }
+    else {
+      $carry .= "{$word}1";
+      $aux[$word] = 1;
+    }
+
+    return $carry;
+  });
+}
+```
---
+++ 2023/challenge01/script.php
@@ -0,0 +1,24 @@
+<?php
+
+function countWords($message) {
+  $info = explode(" ", $message);
+  $aux = [];
+  return array_reduce($info, function ($carry, $word) use (&$aux) {
+    $word = strtolower($word);
+
+    if (isset($aux[$word])) {
+      $carry = str_replace("{$word}{$aux[$word]}", $word . $aux[$word] + 1, $carry);
+      $aux[$word] += 1;
+    }
+    else {
+      $carry .= "{$word}1";
+      $aux[$word] = 1;
+    }
+
+    return $carry;
+  });
+}
+
+$message = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon";
+
+echo countWords($message);
